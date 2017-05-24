c9cc78de5f86edf8f742a3829d9f7a4ed9241236
smudgelang <smudge.language@gmail.com>
2017-05-24 19:11:52 +0200

    added function makeTuple(gc, temp, vec)

--- src/runtime/Object.h
+++ src/runtime/Object.h
@@ -224,6 +224,7 @@ namespace sm{
     Object newObject(runtime::GarbageCollector& gc, bool temp = true) noexcept;
     Object makeFunction(Function*) noexcept;
     Object makeList(runtime::GarbageCollector& gc, bool temp, ObjectVec_t vec = ObjectVec_t()) noexcept;
+    Object makeTuple(runtime::GarbageCollector& gc, bool temp, ObjectVec_t vec = ObjectVec_t()) noexcept;
 
     Object makeTrue() noexcept;
     Object makeFalse() noexcept;
